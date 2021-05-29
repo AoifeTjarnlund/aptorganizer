32cf5e00f4a5d0a3681ba01629f71b60cb175069
Aoife Tjärnlund <tjarnlunda@gmail.com>
2021-05-29 19:34:48 +0200

    kinus pussas

--- src/main/java/com/yrgo/aptorganizer/controllers/MessageController.java
+++ src/main/java/com/yrgo/aptorganizer/controllers/MessageController.java
@@ -4,7 +4,6 @@ import com.yrgo.aptorganizer.dao.MessageRepository;
 import com.yrgo.aptorganizer.domain.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,11 +14,6 @@ public class MessageController {
 
     @Autowired
     private MessageRepository repo;
-
-  //  @GetMapping("/message/{id}")
-   // public Message msg(@PathVariable("id") int id){
-    //    return repo.findById(id).get();
-    //}
 
     @GetMapping("/message/all")
     public List<Message> msg(){
